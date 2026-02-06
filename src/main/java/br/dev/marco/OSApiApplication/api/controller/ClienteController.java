59ae352b86afa1d5081a472e50486a3ab3fe4064
Marco <marcoantoniosouzasantos888@gmail.com>
2026-02-06 10:52:51 -0300

    Inicio da fase 3

--- src/main/java/br/dev/marco/OSApiApplication/api/controller/ClienteController.java
+++ src/main/java/br/dev/marco/OSApiApplication/api/controller/ClienteController.java
@@ -1,24 +1,21 @@
 package br.dev.marco.OSApiApplication.api.controller;
 
 import br.dev.marco.OSApiApplication.domain.model.Cliente;
-import java.util.ArrayList;
+import br.dev.marco.OSApiApplication.domain.repository.ClienteRepository;
 import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class ClienteController 
-{
-    List<Cliente> listaClientes;
-    
-    
+public class ClienteController {
+
+    @Autowired
+    private ClienteRepository clienteRepository;
+
     @GetMapping("/clientes")
-    public String listas() {
-        listaClientes = new ArrayList<Cliente>();
-        listaClientes.add(new Cliente(1, "KGe", "kge@teste.com", "11-99999-9999"));
-        listaClientes.add(new Cliente(1, "Maria", "maria@teste.com","11-88888-8888"));
-        listaClientes.add(new Cliente(1, "Joao", "joao@teste.com","11-77777-7777")); 
-        return "teste api";
+    public List<Cliente> listas() {
+        return clienteRepository.findALL();
     }
-    
+
 }
--- src/main/java/br/dev/marco/OSApiApplication/domain/model/Cliente.java
+++ src/main/java/br/dev/marco/OSApiApplication/domain/model/Cliente.java
@@ -1,11 +1,24 @@
 package br.dev.marco.OSApiApplication.domain.model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Cliente {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
     private String nome;
     private String email;
+@Column(name = "telefone")
     private String fone;
+
     public Cliente() {
+        // Construtor Default
     }
 
     public Cliente(long id, String nome, String email, String fone) {
